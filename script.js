47564e86a9d938b68398afb4f8a3416cae1f3304
Anushka Ghosh <8banushkaghosh@gmail.com>
2026-06-27 16:23:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
- 
+ function checkURL(){
+
+let url=document.getElementById("urlInput").value;
+
+let result=document.getElementById("result");
+
+if(url==""){
+    result.innerHTML="Please enter a URL";
+    result.style.color="orange";
+    return;
+}
+
+let suspicious=0;
+
+// IP Address
+if(/\d+\.\d+\.\d+\.\d+/.test(url))
+    suspicious++;
+
+// @ Symbol
+if(url.includes("@"))
+    suspicious++;
+
+// Too many hyphens
+if((url.match(/-/g)||[]).length>2)
+    suspicious++;
+
+// Long URL
+if(url.length>75)
+    suspicious++;
+
+// HTTPS check
+if(!url.startsWith("https://"))
+    suspicious++;
+
+if(suspicious>=3){
+    result.innerHTML="⚠️ High Risk - Possible Phishing Website";
+    result.style.color="red";
+}
+else if(suspicious>=1){
+    result.innerHTML="⚠️ Suspicious Website";
+    result.style.color="orange";
+}
+else{
+    result.innerHTML="✅ Looks Safe";
+    result.style.color="green";
+}
+}
